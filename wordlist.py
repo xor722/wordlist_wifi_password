8a3bc83321aaf152a269ffa7621b34f262365f62
xor722 <93961632+xor722@users.noreply.github.com>
2024-10-31 11:02:13 +0700

    wifi_pass_wordlist.py

--- wordlist.py
+++ wordlist.py
@@ -4,21 +4,34 @@ import colorama
 
 colorama.init()
 f = Figlet(font='big')
-print(colorama.Fore.GREEN + f.renderText('xor 722'))
 
-profiles = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')[1:]
+output_file = 'wifi_passwords.txt'
+with open(output_file, 'w') as f_out:
+    # Menampilkan dan menyimpan teks awal
+    header_text = colorama.Fore.GREEN + f.renderText('xor 722')
+    print(header_text)
+    f_out.write(header_text + "\n")
 
-profile_names = [i.split(':')[1][1:-1] for i in profiles if "All User Profile" in i]
+    # Mengambil daftar profil Wi-Fi
+    profiles = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')[1:]
+    profile_names = [i.split(':')[1][1:-1] for i in profiles if "All User Profile" in i]
 
-print("{:<30}| {:<}".format("Wi-Fi Name", "Password"))
-print("-----------------------------------------------")
+    f_out.write("{:<30}| {:<}\n".format("Wi-Fi Name", "Password"))
+    f_out.write("-----------------------------------------------\n")
 
-for name in profile_names:
-    try:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', name, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
-        password = [line.split(':')[1][1:-1] for line in results if "Key Content" in line][0]
+    print("{:<30}| {:<}".format("Wi-Fi Name", "Password"))
+    print("-----------------------------------------------")
 
-        print("{:<30}| {:<}".format(name, password))
+    for name in profile_names:
+        try:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', name, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
+            password = [line.split(':')[1][1:-1] for line in results if "Key Content" in line][0]
 
-    except:
-        print("{:<30}| {:<}".format(name, "Not Found")) 
+            output_line = "{:<30}| {:<}".format(name, password)
+            print(output_line)
+            f_out.write(output_line + "\n")
+
+        except Exception as e:
+            output_line = "{:<30}| {:<}".format(name, "Not Found")
+            print(output_line)
+            f_out.write(output_line + "\n")
